806bdd0184c99dcc9ee8c540fa22ccdf9ef5e269
gustcustodio <gustcustodio@gmail.com>
2025-03-04 13:43:31 -0300

    feat: aula 25 - entrada de dados

---
+++ src/estrutura/sequencial/Aula26EntradaDeDados.java
@@ -0,0 +1,33 @@
+package estrutura.sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula26EntradaDeDados {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        //        String x;
+        //        int x;
+        //        double x;
+        //        char x;
+        String x;
+        int y;
+        double z;
+
+        //        x = sc.next(); -> para strings
+        //        x = sc.nextInt(); -> para int
+        //        x = sc.nextDouble(); -> para double
+        //        x = sc.next().charAt(0); -> para char
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+
+        System.out.println("Dados digitados: ");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        sc.close();
+    }
+}
