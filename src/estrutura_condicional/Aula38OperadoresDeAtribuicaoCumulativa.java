88643890e084f2b507a570d837bdef262c8c242b
gustcustodio <gustcustodio@gmail.com>
2025-03-05 06:57:48 -0300

    feat: aula 38 - operadores de atribuição cumulativa

---
+++ src/estrutura_condicional/Aula38OperadoresDeAtribuicaoCumulativa.java
@@ -0,0 +1,23 @@
+package estrutura_condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula38OperadoresDeAtribuicaoCumulativa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de minutos: ");
+        int minutos = sc.nextInt();
+        double conta = 50.0;
+
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0;
+        }
+
+        System.out.printf("Valor da conta = R$%.2f", conta);
+
+        sc.close();
+    }
+}
