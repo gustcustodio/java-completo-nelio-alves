4290d82794e63576561e54fa7378bdd4069f1c1f
gustcustodio <gustcustodio@gmail.com>
2025-03-09 14:04:13 -0300

    feat: aula 70 - exercícios de fixação - exercício 01

---
+++ src/secao08IntroducaoPOO/exercicios/Exercicio01.java
@@ -0,0 +1,27 @@
+package secao08IntroducaoPOO.exercicios;
+
+import secao08IntroducaoPOO.entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        System.out.print("Width: ");
+        rectangle.width = sc.nextDouble();
+        System.out.print("Height: ");
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("AREA = " + String.format("%.2f", rectangle.area()));
+        System.out.println("PERIMETER = " + String.format("%.2f", rectangle.perimeter()));
+        System.out.println("DIAGONAL = " + String.format("%.2f", rectangle.diagonal()));
+
+        sc.close();
+    }
+}
