09387523bab3f9d47ff2a2eaea81d045af1b1eb3
gustcustodio <gustcustodio@gmail.com>
2025-03-10 19:40:45 -0300

    feat: aula 70 - exercícios de fixação - exercício 03

--- src/secao08IntroducaoPOO/entities/Student.java
+++ src/secao08IntroducaoPOO/entities/Student.java
@@ -2,5 +2,13 @@ package secao08IntroducaoPOO.entities;
 
 public class Student {
     public String name;
-    public double notaPrimeiroTrimestre, notaSegundoTrimestre, notaTerceiroTrimestre;
+    public double grade1, grade2, grade3;
+
+    public double finalGrade() {
+        return grade1 + grade2 + grade3;
+    }
+
+    public double missingPoints() {
+        return 60.00 - finalGrade();
+    }
 }
--- src/secao08IntroducaoPOO/exercicios/Exercicio03.java
+++ src/secao08IntroducaoPOO/exercicios/Exercicio03.java
@@ -12,6 +12,25 @@ public class Exercicio03 {
 
         Student student = new Student();
 
+        System.out.print("Digite o nome do aluno: ");
+        student.name = sc.nextLine();
+        System.out.print("Informe a nota do primeiro trimestre: ");
+        student.grade1 = sc.nextDouble();
+        System.out.print("Informe a nota do segundo trimestre: ");
+        student.grade2 = sc.nextDouble();
+        System.out.print("Informe a nota do terceiro trimestre: ");
+        student.grade3 = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE = %.2f", student.finalGrade());
+        System.out.println();
+
+        if (student.finalGrade() < 60.0) {
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS", student.missingPoints());
+        } else {
+            System.out.println("PASS");
+        }
+
         sc.close();
     }
 }
