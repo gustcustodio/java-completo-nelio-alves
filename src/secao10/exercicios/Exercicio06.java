f53de2fcd2e5b4da3c60b1bf127ebc1875f8e314
gustcustodio <gustcustodio@gmail.com>
2025-03-18 06:54:43 -0300

    feat: aula 91 - exercícios de fixação - exercício 06

---
+++ src/secao10/exercicios/Exercicio06.java
@@ -0,0 +1,34 @@
+package secao10.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos valores vai ter cada vetor? ");
+        int n = sc.nextInt();
+
+        int[] vectA = new int[n];
+        int[] vectB = new int[n];
+        int[] vectC = new int[n];
+
+        System.out.println("Digite os valores do vetor A: ");
+        for (int i = 0; i < vectA.length; i++) {
+            vectA[i] = sc.nextInt();
+        }
+
+        System.out.println("Digite os valores do vetor B: ");
+        for (int i = 0; i < vectB.length; i++) {
+            vectB[i] = sc.nextInt();
+        }
+
+        System.out.println("Vetor resultante: ");
+        for (int i = 0; i < vectC.length; i++) {
+            vectC[i] = vectA[i] + vectB[i];
+            System.out.println(vectC[i]);
+        }
+
+        sc.close();
+    }
+}
