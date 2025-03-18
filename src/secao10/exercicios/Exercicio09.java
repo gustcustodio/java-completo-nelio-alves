bc1ad449b37070944905edb78ad6ad7d97956be6
gustcustodio <gustcustodio@gmail.com>
2025-03-18 19:46:35 -0300

    feat: aula 91 - exercícios de fixação - exercício 09

--- src/secao10/exercicios/Exercicio09.java
+++ src/secao10/exercicios/Exercicio09.java
@@ -8,13 +8,27 @@ public class Exercicio09 {
 
         System.out.print("Quantas pessoas você vai digitar? ");
         int n = sc.nextInt();
+        sc.nextLine();
 
+        int older = 0, indexOlder = 0;
         int[] ages = new int[n];
         String[] names = new String[n];
 
-        for(int i = 0; i < n; i++) {
-            System.out.printf("Dados da %da pessoa:");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Dados da %da pessoa: \n", i + 1);
+            System.out.print("Nome: ");
+            names[i] = sc.nextLine();
+            System.out.print("Idade: ");
+            ages[i] = sc.nextInt();
+            sc.nextLine();
+
+            if (ages[i] > older) {
+                older = ages[i];
+                indexOlder = i;
+            }
         }
+
+        System.out.println("Pessoa mais velha: " + names[indexOlder]);
 
         sc.close();
     }
