a893b8393012a8ccc06bccec7490c2b0fc5e8c03
gustcustodio <gustcustodio@gmail.com>
2025-03-27 06:50:51 -0300

    feat: aula 123 - exercício 1 - parte 1

--- src/secao12/exercise/entities/Worker.java
+++ src/secao12/exercise/entities/Worker.java
@@ -2,6 +2,7 @@ package secao12.exercise.entities;
 
 import secao12.exercise.entities.enums.WorkerLevel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Worker {
@@ -10,5 +11,71 @@ public class Worker {
     private Double baseSalary;
 
     private Departament departament;
-    private List<HourContract> contracts;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.departament = departament;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Departament getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(Departament departament) {
+        this.departament = departament;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+    public double income(Integer year, Integer month) {
+        double sum = baseSalary;
+        for (HourContract contract : contracts) {
+            int contract_year = contract.getDate().getYear();
+            int contract_monnth = contract.getDate().getMonthValue();
+            if (year == contract_year && month == contract_monnth) {
+                sum += contract.totalValue();
+            }
+        }
+        return sum;
+    }
 }
