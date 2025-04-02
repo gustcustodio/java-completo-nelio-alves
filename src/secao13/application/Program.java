5cdf9caa9339e4dbea4ae8a4916b044a815dfa3d
gustcustodio <gustcustodio@gmail.com>
2025-04-02 07:04:49 -0300

    feat: aula 132 - sobreposição, palavra super, anotação @Override

--- src/secao13/application/Program.java
+++ src/secao13/application/Program.java
@@ -6,34 +6,50 @@ import secao13.entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
-        Account acc = new Account(1001, "Alex", 0.0);
-        BusinessAccount bacc =
-                new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        //        Account acc = new Account(1001, "Alex", 0.0);
+        //        BusinessAccount bacc =
+        //                new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
-        // UPCASTING
+        // ! UPCASTING ! //
 
-        Account acc1 = bacc;
-        Account acc2 =
-                new BusinessAccount(1003, "Bob", 0.0, 200.0);
-        Account acc3 =
-                new SavingsAccount(1004, "Anna", 0.0, 0.01);
+        //        Account acc1 = bacc;
+        //        Account acc2 =
+        //                new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        //        Account acc3 =
+        //                new SavingsAccount(1004, "Anna", 0.0, 0.01);
 
-        // DOWNCASTING
+        // ! DOWNCASTING ! //
 
-        BusinessAccount acc4 = (BusinessAccount) acc2;
-        acc4.loan(100.0);
+        //        BusinessAccount acc4 = (BusinessAccount) acc2;
+        //        acc4.loan(100.0);
 
         // BusinessAccount acc5 = (BusinessAccount)acc3;
-        if (acc3 instanceof BusinessAccount) {
-            BusinessAccount acc5 = (BusinessAccount) acc3;
-            acc5.loan(200.0);
-            System.out.println("Loan!");
-        }
+        //        if (acc3 instanceof BusinessAccount) {
+        //            BusinessAccount acc5 = (BusinessAccount) acc3;
+        //            acc5.loan(200.0);
+        //            System.out.println("Loan!");
+        //        }
+        //
+        //        if (acc3 instanceof SavingsAccount) {
+        //            SavingsAccount acc5 = (SavingsAccount) acc3;
+        //            acc5.updateBalance();
+        //            System.out.println("Update!");
+        //        }
 
-        if (acc3 instanceof SavingsAccount) {
-            SavingsAccount acc5 = (SavingsAccount) acc3;
-            acc5.updateBalance();
-            System.out.println("Update!");
-        }
+        // ! OVERRIDE ! //
+
+        Account acc5 = new Account(1001, "Alex", 1000.0);
+        acc5.withdraw(200.0);
+        System.out.println(acc5.getBalance());
+
+        Account acc6 =
+                new SavingsAccount(1002, "Maria", 1000.0, 0.1);
+        acc6.withdraw(200.0);
+        System.out.println(acc6.getBalance());
+
+        Account acc7 =
+                new BusinessAccount(1003, "Joao", 1000.0, 500.0);
+        acc7.withdraw(200.0);
+        System.out.println(acc7.getBalance());
     }
 }
