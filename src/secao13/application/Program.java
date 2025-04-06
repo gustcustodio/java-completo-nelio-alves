48d7ff6fd909714139d25647ddfd04b8d60451e1
gustcustodio <gustcustodio@gmail.com>
2025-04-06 12:57:35 -0300

    feat: aula 137 - classes abstratas

--- src/secao13/application/Program.java
+++ src/secao13/application/Program.java
@@ -4,8 +4,13 @@ import secao13.entities.Account;
 import secao13.entities.BusinessAccount;
 import secao13.entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         //        Account acc = new Account(1001, "Alex", 0.0);
         //        BusinessAccount bacc =
         //                new BusinessAccount(1002, "Maria", 0.0, 500.0);
@@ -54,15 +59,73 @@ public class Program {
 
         // ! POLIMORFISMO ! //
 
-        Account x =
-                new Account(1020, "Alex", 1000.0);
-        Account y =
-                new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        //        Account x =
+        //                new Account(1020, "Alex", 1000.0);
+        //        Account y =
+        //                new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        //
+        //        x.withdraw(50.0);
+        //        y.withdraw(50.0);
+        //
+        //        System.out.println(x.getBalance());
+        //        System.out.println(y.getBalance());
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+        // ! CLASSES ABSTRATAS ! //
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        //        Account acc1 = new Account(1001, "Alex", 1000.0);
+        //        Account acc2 = new SavingsAccount(
+        //                1002,
+        //                "Maria",
+        //                1000.0,
+        //                0.01
+        //        );
+        //        Account acc3 = new BusinessAccount(
+        //                1003,
+        //                "Bob",
+        //                1000.0,
+        //                500.0
+        //        );
+
+        List<Account> accounts = new ArrayList<>();
+
+        accounts.add(new SavingsAccount(
+                1001,
+                "Alex",
+                500.00,
+                0.01
+        ));
+        accounts.add(new BusinessAccount(
+                1002,
+                "Maria",
+                1000.0,
+                400.0
+        ));
+        accounts.add(new SavingsAccount(
+                1004,
+                "Bob",
+                300.0,
+                0.01
+        ));
+        accounts.add(new BusinessAccount(
+                1005,
+                "Anna",
+                500.0,
+                500.0
+        ));
+
+        double sum = 0.0;
+        for (Account account : accounts) {
+            sum += account.getBalance();
+        }
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account account : accounts) {
+            account.deposit(10.0);
+            System.out.printf(
+                    "Updated balance for account %d: %.2f%n",
+                    account.getNumber(),
+                    account.getBalance()
+            );
+        }
     }
 }
