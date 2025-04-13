e94a41a30d18d02cd6d6ea936e2881cfad73e075
gustcustodio <gustcustodio@gmail.com>
2025-04-13 14:21:47 -0300

    feat: aula 158 - FileWriter e BufferedWriter

---
+++ src/secao15/Aula158.java
@@ -0,0 +1,21 @@
+package secao15;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Aula158 {
+    public static void main(String[] args) {
+        String[] lines = new String[]{"Good morning", "Good afternoon", "Good night"};
+        String path = "C:\\temp\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
