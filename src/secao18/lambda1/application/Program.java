6c55d8a6068ef140cefe1ca00d5e0c699a20556d
gustcustodio <gustcustodio@gmail.com>
2025-06-29 13:06:03 -0300

    feat: aula 193 - uma experiência com Comparator (Lambda 2)

--- src/secao18/lambda1/application/Program.java
+++ src/secao18/lambda1/application/Program.java
@@ -14,9 +14,8 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comparator = (p1, p2) -> {
-            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
+        Comparator<Product> comparator = (p1, p2) ->
+                p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
         list.sort(comparator);
 
