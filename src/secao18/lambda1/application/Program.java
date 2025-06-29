d456562303aee2707e5b4e8bc971189fc553dbab
gustcustodio <gustcustodio@gmail.com>
2025-06-29 13:06:47 -0300

    feat: aula 193 - uma experiência com Comparator (Lambda 3)

--- src/secao18/lambda1/application/Program.java
+++ src/secao18/lambda1/application/Program.java
@@ -3,7 +3,6 @@ package secao18.lambda1.application;
 import secao18.lambda1.entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -14,10 +13,8 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comparator = (p1, p2) ->
-                p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-        list.sort(comparator);
+        list.sort((p1, p2) ->
+                p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product p : list) {
             System.out.println(p);
