2a1a586bc9a3e7c5f97b1bfbd91c9507581c9841
gustcustodio <gustcustodio@gmail.com>
2025-07-09 06:58:16 -0300

    feat: aula 219 - classe Seller

--- src/secao19/projeto/application/Program.java
+++ src/secao19/projeto/application/Program.java
@@ -1,10 +1,21 @@
 package secao19.projeto.application;
 
 import secao19.projeto.model.entities.Department;
+import secao19.projeto.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(21,
+                "Bob",
+                "bob@gmail.com",
+                new Date(),
+                3000.0,
+                department);
+        System.out.println(seller);
     }
 }
